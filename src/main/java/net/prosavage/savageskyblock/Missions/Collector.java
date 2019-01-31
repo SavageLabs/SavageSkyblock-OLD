983ac828edfd5816e0f0413500a22ce265156d6c
Joeyvdh99 <45742791+Joeyvdh99@users.noreply.github.com>
2019-02-01 00:10:53 +0100

    Update Collector.java

--- src/main/java/net/prosavage/savageskyblock/Missions/Collector.java
+++ src/main/java/net/prosavage/savageskyblock/Missions/Collector.java
@@ -18,12 +18,13 @@ public class Collector {
 
     public ItemStack getItem() {
         ArrayList<String> lore = new ArrayList<>();
-        lore.add(ChatColor.translateAlternateColorCodes('&', "&bComplete island missions to gain crystals that can be spent on Boosters and Upgrades."));
+        lore.add(ChatColor.translateAlternateColorCodes('&', "&7Complete island missions to gain crystals"));
+        lore.add(ChatColor.translateAlternateColorCodes('&', "&7that can be spent on Boosters and Upgrades."));
         lore.add("");
         lore.add(ChatColor.translateAlternateColorCodes('&', "&b&lInformation:"));
-        lore.add(ChatColor.translateAlternateColorCodes('&', "&b&l* &7Objective: &bCollect 7,500 Ores"));
-        lore.add(ChatColor.translateAlternateColorCodes('&', "&b&l* &7Current Status: &b" + current + "/7500"));
-        lore.add(ChatColor.translateAlternateColorCodes('&', "&b&l* &7Reward: &b" + reward));
+        lore.add(ChatColor.translateAlternateColorCodes('&', "&b&l * &7Objective: &bCollect 7,500 Ores"));
+        lore.add(ChatColor.translateAlternateColorCodes('&', "&b&l * &7Current Status: &b" + current + "/7500"));
+        lore.add(ChatColor.translateAlternateColorCodes('&', "&b&l * &7Reward: &b" + reward));
         lore.add("");
         lore.add(ChatColor.translateAlternateColorCodes('&', "&b&l[!] &bComplete this mission for rewards."));
         return SavageSkyBlock.getSkyblock.makeItem(Material.DIAMOND_ORE, 1, 0, "&b&lCollector", lore);
